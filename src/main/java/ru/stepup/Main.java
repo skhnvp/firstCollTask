f2d6e04af2ff7a676bc18d691925db194089e755
Pavel <skhnvp@gmail.com>
2025-04-02 23:47:33 +0300

    init

---
+++ src/main/java/ru/stepup/Main.java
@@ -0,0 +1,35 @@
+package ru.stepup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> l = new ArrayList<>();
+
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Введите четное число: ");
+            int num = s.nextInt();
+
+            if (num % 2 == 0) {
+                for (Integer i = 1; i <= num; i++) {
+                    l.add(i);
+                }
+
+                Integer temp = 0;
+
+                for (int i = 0; i < l.size(); i++) {
+                    temp = l.get(i);
+
+                    l.set(i,l.get(++i));
+                    l.set(i,temp);
+                }
+            } else {
+                throw new IllegalArgumentException("Число должно быть четным");
+            }
+            System.out.println(l);
+        }
+    }
+}
